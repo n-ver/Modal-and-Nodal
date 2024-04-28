95432b1591aa42e338a804834f74328c05d1b899
Nata <nata@verthein.us>
2024-04-28 18:16:52 -0500

    compiles

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-mod login;
+use thirtyfour::prelude::*;
+use tokio;
+
 #[tokio::main]
-async fn main() {
-    // assuming your async function in `scraper.rs` is called `run`
-    if let Err(e) = scraper::run().await {
-        eprintln!("Failed to execute scraper: {}", e);
-    }
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let mut caps = DesiredCapabilities::chrome();
+    caps.add_chrome_arg("--enable-automation")?;
+    let driver = WebDriver::new("http://localhost:4444/wd/hub", &caps).await?;
+
+    driver.get("https://wikipedia.org").await?;
+
+    Ok(())
 }
