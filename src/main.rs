13073a4de4906dd804ded301b0d4828c6fa99d7f
Nata <nata@verthein.us>
2024-04-28 19:31:15 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,33 @@ async fn main() -> WebDriverResult<()> {
     let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:9515", &caps).await?;
 
-    // Navigate to https://wikipedia.org.
-    driver.get("https://us.prairielearn.com/pl/auth/institution/3/saml/login").await?;
+    // Navigate to log in page
+    driver.get("http://www.library.illinois.edu/proxy/go.php?url=http://www.lordisco.com/tjd/CoverFrame").await?;
+    // let it load
     tokio::time::sleep(Duration::from_secs(1)).await;
-    let elem_form = driver.find_element(By::Id("i0116")).await?;
+
+    let email_form = driver.find_element(By::Id("i0116")).await?;
     
     // Find element from element.
     // let elem_text = elem_form.find_element(By::Id("searchInput")).await?;
 
     // Type in the search terms.
     let UIUC_User : String = env::var("UIUC_User").unwrap_or_else(|_| "Unknown user".to_string());
-    elem_form.send_keys(UIUC_User).await?;
+    email_form.send_keys(UIUC_User).await?;
 
-    // Click the search button.
-    // let elem_button = elem_form.find_element(By::Css("button[type='submit']")).await?;
-    // elem_button.click().await?;
+    // Click the next button.
+    let next_button = driver.find_element(By::Id("idSIButton9")).await?;
+    next_button.click().await?;
 
+    // Type in password
+    let UIUC_Pass : String = env::var("UIUC_Pass").unwrap_or_else(|_| "Unknown user".to_string());
+    driver.find_element(By::Id("i0118")).await?;
+    let pass_form = driver.find_element(By::Id("i0118")).await?;
+    pass_form.send_keys(UIUC_Pass).await?;
+    driver.find_element(By::Id("idSIButton9")).await?;
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    let next_button2 = driver.find_element(By::Id("idSIButton9")).await?;
+    next_button2.click().await?;
     // Look for header to implicitly wait for the page to load.
     // driver.find_element(By::ClassName("firstHeading")).await?;
     // assert_eq!(driver.title().await?, "Selenium - Wikipedia");
