746dac33dbf8bd93bd383d296573443233e4d39f
Nver <av45@illinois.edu>
2024-05-04 16:46:16 -0500

    Update main.rs
    Fixed all previous issues and bugs.  Fixed thread messaging, speed issues, and upload delay.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use mongodb::Database;
 use mongodb::Collection;
 use bson::Document;
 use tokio::sync::mpsc;
+use thirtyfour::ChromeCapabilities;
 
 
 struct MusData {
@@ -36,9 +37,8 @@ impl fmt::Display for MusData {
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    
-    let client_uri = "mongodb+srv://Jazz_Musician_Scraper:TzErtYpD74ueEQ1I@cluster0.wyv1qyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    // let client_uri = env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!"); not working
+    env::set_var("RUST_BACKTRACE", "1");
+    let client_uri = env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!"); not working
 
     // A Client is needed to connect to MongoDB:
     // An extra line of code to work around a DNS issue on Windows:
@@ -58,14 +58,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
         receive_from_scraper(rx, &database).await;
     });
     
-    scraper_handle.await?;
-    upload_handle.await?;
+    let result = tokio::try_join!(scraper_handle, upload_handle);
+    result?;
+
     Ok(())
 }
 // Gathers data
 async fn scraper(tx: mpsc::Sender<MusData>) -> Result<(), Box<dyn Error>> {
     let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:9515", &caps).await?;
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
 
     // Navigate to log in page
 
@@ -75,9 +76,6 @@ async fn scraper(tx: mpsc::Sender<MusData>) -> Result<(), Box<dyn Error>> {
 
     let email_form = driver.find_element(By::Id("i0116")).await?;
     
-    // Find element from element.
-    // let elem_text = elem_form.find_element(By::Id("searchInput")).await?;
-
     // Type in the search terms.
     let UIUC_User : String = env::var("UIUC_User").unwrap_or_else(|_| "Unknown user".to_string());
     email_form.send_keys(UIUC_User).await?;
@@ -87,8 +85,7 @@ async fn scraper(tx: mpsc::Sender<MusData>) -> Result<(), Box<dyn Error>> {
     next_button.click().await?;
 
     // Type in password
-    // let UIUC_pass : String = env::var("UIUC_Pass").unwrap_or_else(|_| "Unknown user".to_string());
-    let UIUC_pass = "Rc050109#123";
+    let UIUC_pass : String = env::var("UIUC_Pass").unwrap_or_else(|_| "Unknown user".to_string());
     driver.find_element(By::Id("i0118")).await?;
     tokio::time::sleep(Duration::from_secs(1)).await;
     let pass_form = driver.find_element(By::Id("i0118")).await?;
@@ -97,55 +94,45 @@ async fn scraper(tx: mpsc::Sender<MusData>) -> Result<(), Box<dyn Error>> {
     tokio::time::sleep(Duration::from_secs(1)).await;
     let next_button2 = driver.find_element(By::Id("idSIButton9")).await?;
     next_button2.click().await?;
-    // Look for header to implicitly wait for the page to load.
-    // driver.find_element(By::ClassName("firstHeading")).await?;
-    // assert_eq!(driver.title().await?, "Selenium - Wikipedia");
-
     tokio::time::sleep(Duration::from_secs(5)).await;
-    // let search_button = WebDriverWait::new(&driver, std::time::Duration::from_secs(10))
-    //     .until(|d| d.find_element(By::Css("div.musician #searchButtonId")));
-    // search_button.click().await?;
     let search_button = driver.find_element(By::Css("a[href='MusicianSearch?dest=MusicianIndex'] > .nav-button.musician-element")).await?;
     search_button.click().await?;
     let search_button = driver.find_element(By::Css("input[name='action2'][value='Search']")).await?;
     search_button.click().await?;
-    let link = driver.find_element(By::Css("a[href*='nav=down']")).await?;
-    link.click().await?;
-    let table = driver.find_element(By::Css("table.index")).await?;
-    let rows = table.find_elements(By::Css("tr")).await?;
     let mut musicians_with_more_than_two_sessions : Vec<MusData> = Vec::new();
-    
-    for row in rows.iter().skip(1) { // Skip header row
-        let cells = row.find_elements(By::Css("td")).await?;
-        if cells.len() > 2 { // Ensure there are enough columns in the row
-            let id = row.get_attribute("id").await?.unwrap_or_else(|| "id failed".to_string());
-            let musician_name = cells[0].text().await?; // Index might need adjustment
-            let years = cells[1].text().await?;
-            let instruments = cells[2].text().await?;
-            let session_count: i32 = cells[3].text().await?.parse().unwrap_or(0); // Index might need adjustment
-    
-            if session_count > 2 {
-                let data: MusData = MusData {
-                    id: id,
-                    name: musician_name,
-                    instruments: instruments,
-                    years: years,
-                    sessions: session_count,
-                };
-                tx.send(data).await.expect("Failed to send data");
+    let mut page_num : i64 = 1;
+    loop {
+        println!("on page {}", page_num);
+        let bottom_buttons = driver.find_element(By::Css(".bottom_buttons")).await?;
+        if let Ok(next_page_button) = bottom_buttons.find_element(By::Css("a[href*='nav=down']")).await {
+            let table = driver.find_element(By::Css("table.index")).await?;
+            let rows = table.find_elements(By::Css("tr")).await?;
+            for row in rows.iter().skip(1) { // Skip header row
+                let cells = row.find_elements(By::Css("td")).await?;
+                if cells.len() > 2 { // Ensure there are enough columns in the row
+                    let session_count: i32 = cells[3].text().await?.parse().unwrap_or(0);
+                    if session_count >= 25 { // ensure musician meets minimum requirement
+                        let data: MusData = MusData {
+                            id: row.get_attribute("id").await?.unwrap_or_else(|| "id failed".to_string()),
+                            name: cells[0].text().await?,
+                            instruments: cells[2].text().await?,
+                            years: cells[1].text().await?,
+                            sessions: session_count,
+                        };
+                        
+                        tx.send(data).await.expect("Failed to send data");
+                    }
+                }
             }
+            page_num = page_num + 1;
+            next_page_button.click().await?;
+        } else {
+            break;
         }
     }
 
-    // Output the musicians
-    println!("let v = vect![");
-    for musician in musicians_with_more_than_two_sessions {
-        println!("MusData {{ \nid : \"{}\",\nname : \"{}\",\ninstruments : \"{}\",\nyears : \"{}\",\nsessions : {},\n}},", musician.id, musician.name, musician.instruments, musician.years, musician.sessions);
+        return Ok(())
     }
-    println!("];");
-    Ok(())
-}
-
 // Uploads to database
 async fn upload(database: &Database, data: MusData) -> Result<(), Box<dyn Error>> {
     // filters into correct collection
@@ -172,23 +159,23 @@ async fn upload(database: &Database, data: MusData) -> Result<(), Box<dyn Error>
 }
 async fn receive_from_scraper(mut receiver: mpsc::Receiver<MusData>, database: &Database) -> Result<(), Box<dyn Error>> {
     while let Some(data) = receiver.recv().await {
+        println!("MusData {{ \nid : \"{}\",\nname : \"{}\",\ninstruments : \"{}\",\nyears : \"{}\",\nsessions : {},\n}},", data.id, data.name, data.instruments, data.years, data.sessions);
         upload(&database, data).await?;
     }
     Ok(())
 }
-// mongodb+srv://Jazz_Musician_Scraper:TzErtYpD74ueEQ1I@cluster0.wyv1qyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 // inserts if not already in database
 async fn insert_or_not(collection: Collection<Document>, data: MusData)  -> Result<(), Box<dyn Error>> { 
-    
+    println!("insert or not running");
     let already_exists: Option <Document> = collection.find_one(
         doc! {
               "id": data.id.clone(),
         },
         None,
      ).await?;
-     
      if already_exists.is_none() { // entry does not exist
+        println!("making new entry");
         let new_doc = doc! {
             "id" : data.id,
             "name" : data.name,
@@ -198,9 +185,5 @@ async fn insert_or_not(collection: Collection<Document>, data: MusData)  -> Resu
         };
     let insert_result = collection.insert_one(new_doc.clone(), None).await?;
      } 
-     Ok(()) // didn't exist
+     Ok(())
 }
-// cedar walton bolivia
-// driftin
-// flintstones
-// the sequel ??? miller
